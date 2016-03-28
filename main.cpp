a25233a3a030a73d58838df913bffc6569cd0a5d
fgodoys <felipesilva349@gmail.com>
2016-03-28 10:33:24 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "Pilha.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char character;
+  GenericStack stack;
+  char stackChar;
+  cout << "Enter a string; press return." << endl;
+  cin.get(character);
+  while (character != '\n')
+    {
+      stack.push(character);
+      cin.get(character);
+    }
+  while (!stack.isEmpty())
+    {
+      stackChar = stack.pop();
+      cout << stackChar;
+    }
+  cout << endl;
+}
