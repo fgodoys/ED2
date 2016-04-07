63e327c2b33c39eb42eb51d9e3c9af8960289067
brunokadri <bruno_kadri@hotmail.com>
2016-04-06 21:58:34 -0300

    Added files via upload

---
+++ HippieSort.cpp
@@ -0,0 +1,92 @@
+#include "Heap.h"
+#include <iostream>
+
+Heap::Heap(int max)
+{
+	items = new ItemType(max);
+	length = 0;
+}
+
+Heap::Heap(int max, ItemType* array, int qnt)
+{
+	int i;
+	for (i = (qnt/2)-1 ; i>=0 ; i--)
+	{
+		descida(i);
+	}
+}
+
+bool Heap::isFull() const{
+	return (maxItems == length);
+}
+
+bool Heap::isEmpty() const{
+	return (length == 0);
+}
+
+void Heap::enqueue(ItemType newItem){
+	items[length] = newItem;
+	if(items[(length - 1) / 2] < items[length]){
+		subida(length);
+	}
+	length++;
+}
+
+ItemType Heap::dequeue(){
+	ItemType item = items[0];
+	items[0] = items[length];
+	length--;
+	descida(0);
+}
+
+void Heap::descida (int pos, int bottom)
+{
+	int aux = items[pos];
+	do
+	{
+		if(items[(2*pos+1)]>items[(2*pos+2)])
+		{
+			items[pos] = items[(2*pos+1)];
+			items[(2*pos+1)] = aux;
+			pos = 2*pos+1;
+		}
+		else
+		{
+			items[pos] = items[(2*pos+2)];
+			items[(2*pos+2)] = aux;
+			pos = 2*pos+2;	
+		}
+	}
+	while ((aux<items[pos])&&(pos<bottom));
+}
+
+void Heap::subida (int pos)
+{
+	int aux = items[pos];
+	do
+	{
+		items[pos] = items[(pos-1)/2];
+		items[(pos-1)/2] = aux;
+		pos = (pos-1)/2;
+	}
+	while(aux>items[(pos-1)/2]);
+}
+
+void Heap::sort ()
+{
+		int aux;
+		int tam = length-1;
+	
+	do
+	{
+		aux = items[0];
+		items[0] = items[tam];
+		items[tam]=aux;
+		tam--;
+		descida(0;tam);		
+	}
+	while(tam>=0);
+}
+	
+	
+}
